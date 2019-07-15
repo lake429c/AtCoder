4d7f7cc44f879d9a6e8292ba90c85f8646d2c977
lake429c <have2nthusiasm@gmail.com>
2019-07-15 21:50:57 +0900

    agc難しい

---
+++ AGC035A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lint = long long int;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((lint)(x).size())
+#define POW2(n) (1LL << (n))
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+
+//二進数に変換
+lint binary(lint bina){
+    int ans = 0;
+    for (int i = 0; bina>0 ; i++)
+    {
+        ans = ans+(bina%2)*pow(10,i);
+        bina = bina/2;
+    }
+    return ans;
+}
+
+int main()
+{
+  lint n;
+  cin >> n;
+  vector<lint> a(n);
+  REP(i,n) cin >> a[i];
+
+  REP(i,n) a[i] = binary(a[i]);
+  return 0;
+}
